528e83b15f0f1239da3c04b332b93ca1dc7b86c9
NishuDissanayake <nishudissanayake5252@gmail.com>
2025-04-28 14:25:48 +0530

    App initi

--- todo-app/src/components/Navbar.tsx
+++
@@ -1,9 +0,0 @@
-function Navbar() {
-    return (
-        <nav className="bg-blue-600 p-4 text-white text-center text-2xl font-bold">
-          Todo Application
-        </nav>
-      );
-}
-
-export default Navbar
