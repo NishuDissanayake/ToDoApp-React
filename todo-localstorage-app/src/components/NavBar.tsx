0f7e7ed672e999a096e5cbb111b2ee2a0274b545
NishuDissanayake <nishudissanayake5252@gmail.com>
2025-04-28 14:37:52 +0530

    Styling update

--- todo-localstorage-app/src/components/NavBar.tsx
+++ todo-localstorage-app/src/components/NavBar.tsx
@@ -1,16 +1,33 @@
 import { Container, Navbar, Nav } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 function NavBar() {
+  const location = useLocation();
+
   return (
-    <Navbar bg="dark" variant="dark" expand="lg" className="mb-4">
+    <Navbar bg="primary" variant="dark" expand="lg" sticky="top" className="shadow-sm mb-4">
       <Container>
-        <Navbar.Brand as={Link} to="/">Todo App</Navbar.Brand>
+        <Navbar.Brand as={Link} to="/" style={{ fontWeight: 'bold', letterSpacing: '1px' }}>
+          Todo App
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav>
-            <Nav.Link as={Link} to="/">Home</Nav.Link>
-            <Nav.Link as={Link} to="/add">Add Task</Nav.Link>
+          <Nav className="ms-auto">
+            <Nav.Link
+              as={Link}
+              to="/"
+              active={location.pathname === '/'}
+              style={{ marginRight: '1rem' }}
+            >
+              Home
+            </Nav.Link>
+            <Nav.Link
+              as={Link}
+              to="/add"
+              active={location.pathname === '/add'}
+            >
+              Add Task
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
